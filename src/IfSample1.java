460b84417bccf3b0d4c478e3d8f0048a67c80d6d
mei-tsubakihara <mei19930519@gmail.com>
2023-01-30 14:54:14 +0900

    Lesson4 までの保存

---
+++ src/IfSample1.java
@@ -0,0 +1,17 @@
+
+public class IfSample1 {
+
+    public static void main(String[] args) {
+        int score = -10; //成績
+        
+        //scoreが60以上ならば
+        if(score >= 60) {
+            //合格です!と表示
+            System.out.println("合格です!");
+        } else {
+            System.out.println("赤点です…");
+        }
+
+    }
+
+}
